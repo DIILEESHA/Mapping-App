a3b857ce248158d9b25d8ee63b27e0f5f8ba239e
DILEESHA1 <dileeshanawarathna18@gmail.com>
2022-07-08 18:48:15 +0530

    setup mapping process

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {render} from 'react-dom';
+import Map, {Marker} from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGlsZWVzaGEiLCJhIjoiY2w1Y2dpOTV5MGg5dTNibXRnbnZ5MThybiJ9.CGapjZ7czC1E_bviZMG4pg'; // Set your mapbox token here
+
+
+function App() {
+  return (
+    <Map
+    initialViewState={{
+      latitude: 37.8,
+      longitude: -122.4,
+      zoom: 14
+    }}
+    style={{width: 800, height: 600}}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+    mapboxAccessToken={MAPBOX_TOKEN}
+  >
+    <Marker longitude={-122.4} latitude={37.8} color="red" />
+  </Map>
+  );
+}
+
+export default App;
