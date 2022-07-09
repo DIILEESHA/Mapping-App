558fe99d5bec07937bad11c6fd92d72d445b063c
DILEESHA1 <dileeshanawarathna18@gmail.com>
2022-07-09 10:42:52 +0530

    Added popup msg

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
-import Map, { Marker } from "react-map-gl";
-
+import * as React from "react";
+import { useEffect } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
+import "./app.css";
 import "mapbox-gl/dist/mapbox-gl.css";
+import StarIcon from "@mui/icons-material/Star";
+import axios from "axios";
+import {format} from "timeago.js"
 
 function App() {
+  const [pinner, setpinner] = React.useState([]);
+  const [showPopup, setShowPopup] = React.useState(true);
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pinner");
+        setpinner(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
   return (
     <Map
       initialViewState={{
@@ -16,18 +36,35 @@ function App() {
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
     >
-      <Marker
-        longitude={84.124008}
-        latitude={	28.394857}
-        color="red"
-        
-      />
-      <Marker
-        longitude={5.124008}
-        latitude={	9.394857}
-        color="red"
-        you are here
-      />
+      {pinner.map((p) => (
+        <>
+          <Marker longitude={p.long} latitude={p.lat} color="red" />
+
+          <Popup longitude={p.long} latitude={p.lat} anchor="left">
+            <div className="mark">
+              <div className="card">
+                <label>Place</label>
+                <h4 className="cr1">{p.title}</h4>
+                <label>Review</label>
+                <p className="cr1">{p.desc}</p>
+                <label>Rating</label>
+                <div className="str">
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                  <StarIcon />
+                </div>
+                <label>Information</label>
+                <span className="user">
+                  Created by <b>{p.username}</b>
+                </span>
+                <span className="userme">{format(p.createdAt)}</span>
+              </div>
+            </div>
+          </Popup>
+        </>
+      ))}
     </Map>
   );
 }
