b84ba4b048039b3fba72c755f21128fca219c81f
DILEESHA1 <dileeshanawarathna18@gmail.com>
2022-07-09 14:50:47 +0530

    new place creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Map, { Marker, Popup } from "react-map-gl";
 import "./app.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import StarIcon from "@mui/icons-material/Star";
 import axios from "axios";
-import {format} from "timeago.js"
+import { format } from "timeago.js";
 
 function App() {
+  const [newPlace, setNewPlace] = useState(null);
   const [pinner, setpinner] = React.useState([]);
   const [showPopup, setShowPopup] = React.useState(true);
+  const [currentPlaceId, setcurrentPlaceId] = useState(null);
 
   useEffect(() => {
     const getPins = async () => {
@@ -23,6 +25,18 @@ function App() {
     getPins();
   }, []);
 
+  const handleMarkerClick = (id) => {
+    setcurrentPlaceId(id);
+  };
+
+  const addClick = (e) => {
+     const [longitude, latitude] = e.lngLat;
+    setNewPlace({
+      lat: latitude,
+      long: longitude,
+    });
+  };
+
   return (
     <Map
       initialViewState={{
@@ -35,36 +49,64 @@ function App() {
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      onDblClick={addClick}
     >
       {pinner.map((p) => (
         <>
-          <Marker longitude={p.long} latitude={p.lat} color="red" />
-
-          <Popup longitude={p.long} latitude={p.lat} anchor="left">
-            <div className="mark">
-              <div className="card">
-                <label>Place</label>
-                <h4 className="cr1">{p.title}</h4>
-                <label>Review</label>
-                <p className="cr1">{p.desc}</p>
-                <label>Rating</label>
-                <div className="str">
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
+          <Marker
+            longitude={p.long}
+            latitude={p.lat}
+            color="red"
+            style={{ cursor: "pointer" }}
+            onClick={() => handleMarkerClick(p._id)}
+          />
+          if () {} else {}
+          {p._id === currentPlaceId && (
+            <Popup
+              longitude={p.long}
+              latitude={p.lat}
+              anchor="left"
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setcurrentPlaceId(null)}
+            >
+              <div className="mark">
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="cr1">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="cr1">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="str">
+                    <StarIcon />
+                    <StarIcon />
+                    <StarIcon />
+                    <StarIcon />
+                    <StarIcon />
+                  </div>
+                  <label>Information</label>
+                  <span className="user">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="userme">{format(p.createdAt)}</span>
                 </div>
-                <label>Information</label>
-                <span className="user">
-                  Created by <b>{p.username}</b>
-                </span>
-                <span className="userme">{format(p.createdAt)}</span>
               </div>
-            </div>
-          </Popup>
+            </Popup>
+          )}
         </>
-      ))}
+        ))}
+        {newPlace && (
+          <Popup
+            longitude={newPlace.long}
+            latitude={newPlace.lat}
+            anchor="left"
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setNewPlace(null)}
+          >
+            hello
+            </Popup>
+      )}
     </Map>
   );
 }
